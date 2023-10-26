ec9f969ae530de58e47de048afade2fef0732cb1
Semil Jain <36932883+SemilJain@users.noreply.github.com>
2023-10-26 01:41:10 -0600

    Update preparation.sh to add logs

--- preparation.sh
+++ preparation.sh
@@ -1,36 +1,56 @@
 #!/bin/bash
 
+# Define the log file path
+LOG_FILE="/local/repository/script_log.txt"
+
+# Function to log a message
+log() {
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
+}
+
+exec > >(tee -a "$LOG_FILE") 2>&1
+log "Starting script..."
+
 # Change permissions for the script
 sudo chmod 755 setup-grow-rootfs.sh
 
 # Set the RESIZEROOT environment variable and run the script
 sudo RESIZEROOT=0 ./setup-grow-rootfs.sh
 
+log "Resizing completed."
+
+
 # Install docker
 if ! command -v docker; then
   # If Docker is not installed, install it
+  log "Installing docker from script..."
   sh get-docker.sh
 fi
 
 # Allow non-root docker access
 if ! getent group docker; then
+  log "Setting non-root access - adding to group..."
   # If the group doesn't exist, create it
   sudo groupadd docker
 fi
 
 if ! groups | grep "docker"; then
   # If the user is not a member, add them to the group
+  log "Adding user to group..."
   sudo usermod -aG docker $USER
   newgrp docker
 fi
 
 # Install Docker compose
+log "Installing Docker-compose and xml-lint"
 sudo apt install docker-compose -y
 
 sudo apt -y install libxml2-utils
 
 
 # Create Repos
+log "Setting up repos..."
 export PROJ_DIR=`pwd`
 export PROJ_VERSION="4.1.0"
 DATASET=$(geni-get manifest | xmllint --xpath "string(//*[local-name()='data_item'])" -)
@@ -43,6 +63,7 @@ curl -SL https://github.com/NCAR/container-dtc-nwp/archive/refs/tags/v${PROJ_VER
 fi
 
 # Get all dockers from HUB
+log "Downloading images from Docker Hub..."
 cd ${PROJ_DIR}
 docker pull dtcenter/wps_wrf:${PROJ_VERSION}
 docker pull dtcenter/gsi:${PROJ_VERSION}
@@ -52,11 +73,11 @@ docker pull dtcenter/nwp-container-met:${PROJ_VERSION}
 docker pull dtcenter/nwp-container-metviewer:${PROJ_VERSION}
 
 if [ "$(docker images | wc -l)" -ne 7 ]; then
-  echo "Error: The number of Docker images is not 7."
+  log "Error: The number of Docker images is not 7."
   exit 1  # Exit with an error code
 fi
 
 cd ${CASE_DIR}
 mkdir -p wpsprd wrfprd gsiprd postprd pythonprd metprd metviewer/mysql
 cd ${PROJ_DIR}
-echo "Preparation done. The script completed successfully."
+log "Preparation done. The script completed successfully."

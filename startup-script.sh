16a7bb1934febeb3ceac09a9c8836501bd35d909
Semil Jain <36932883+SemilJain@users.noreply.github.com>
2023-11-01 13:37:20 -0600

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
-# sudo chmod +x /local/repository/preparation.sh
-# source /local/repository/preparation.sh
-
-# sudo chmod +x /local/repository/sandy.sh
-# source /local/repository/sandy.sh
 export USER="semil"
 cd /users/$USER
 sudo mkdir hello
 sudo cp -r /local/repository/* ./
+
+sudo chmod +x ./preparation.sh
+source ./preparation.sh
+
+sudo chmod +x ./sandy.sh
+source ./sandy.sh
+
 echo "Startup script Completed Successfully!!!"

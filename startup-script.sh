a469452ea4003e8a4998d9a62c1bb5680d829049
Semil Jain <36932883+SemilJain@users.noreply.github.com>
2023-11-01 20:46:57 -0600

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -2,10 +2,10 @@
 cd /users/$USER
 sudo cp -r /local/repository/* ./
 
-chmod +x ./preparation.sh
-sh preparation.sh
+# chmod +x ./preparation.sh
+bash preparation.sh
 
-chmod +x ./sandy.sh
-sh sandy.sh
+# chmod +x ./sandy.sh
+bash sandy.sh
 
 echo "Startup script Completed Successfully!!!"

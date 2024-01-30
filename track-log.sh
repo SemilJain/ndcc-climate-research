33bcca8e2da42bb8854411721ec0867ba00e6c0b
hamzalsheikh <7.amza98@gmail.com>
2024-01-29 19:00:45 -0700

    track log accepts multiple log file entries

--- track-log.sh
+++ track-log.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+echo "track log!"
 echo $1
 
-# to-do check if dir or file ?
-
-# wait till file creation
-until [ -f $1 ]
-do
-  echo "Waiting for the file to be created..."
-  sleep 1
-done
-
-sudo tail -f $1 > "climate-dashboard/static/logs/$1"
+IFS=" " read -ra log_files <<< "$1"
+  for file in ${log_files[@]}; do
+  # wait till file creation
+    until [[ -f $file ]]
+    do
+      echo "Waiting for the file $1 to be created..."
+      sleep 1
+    done
+    
+    sudo tail -f $file > "climate-dashboard/static/logs/${file##*/}" &
+  done
 
 # to-do kill process when done
+
+tailpid=$!
+
+# wait for sometime
+sleep 25
+
+# now kill the tail process
+sudo kill $tailpid

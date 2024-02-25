4363573ebbe7132eb1c7b84ba46a877e907d74b6
Semil Jain <semil@Semils-MacBook-Pro.local>
2024-02-24 17:34:24 -0700

    testing changes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment');
 const app = express();
 const port = 3000;
 const { downloadData, modifyFiles, processData, postProcess } = require('./dataProcessing');
@@ -10,7 +11,18 @@ app.get('/', (req, res) => {
 
 app.post('/processDate', (req, res) => {
   const { data } = req.body;
-  exec('bash /users/geniuser/processDate.sh', (error, stdout, stderr) => {
+  const YMDH = `${data.date}00`
+  const MAX_FHR = 24;
+  const FHR_INC = 3;
+  const DATASET = data.uid;
+  const dateTime = moment(`${data.date} ${data.time}`, 'YYYYMMDD HH:mm:ss');
+  const startDate = dateTime.format('YYYY-MM-DD_HH:mm:ss');
+  dateTime.add(1, 'days');
+  const endDate = dateTime.format('YYYY-MM-DD_HH:mm:ss');
+
+  const command = `bash /users/geniuser/processDate.sh ${YMDH} ${MAX_FHR} ${FHR_INC} ${DATASET} ${startDate} ${endDate} ${data.lat} ${data.lon}`;
+
+  exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error.message}`);
       return;
@@ -22,7 +34,7 @@ app.post('/processDate', (req, res) => {
     console.log(`stdout: ${stdout}`);
   });
 
-  res.send(`Processed data: ${data}`);
+  res.send(`Processing data: ${data}`);
 });
 
 app.listen(port, () => {
